98056df1ad6aadc73847dd204b23008c35bad90e
Vikktoro <vspahi@gmail.com>
2022-03-17 21:37:46 +0100

    project_maven+jenkins

---
+++ mvn_project_jenkins/src/main/java/com/kkjava/mvn_project_jenkins/App.java
@@ -0,0 +1,13 @@
+package com.kkjava.mvn_project_jenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
